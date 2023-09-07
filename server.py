cb7ff3975b5c155ba02fc7fcff34f351d03764e6
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-09-07 10:55:16 +0300

    finished coding

--- server.py
+++ server.py
@@ -2,15 +2,12 @@ voted_per_students= int(input("Her öğrencinin kaç adaya oy vereceğini yazın
 ipv4addr=input("Enter IPv4 Address of the device:")
 from flask import Flask, render_template, request
 import sqlite3 as db
+from sqlite3 import Error
 
 App = Flask(__name__,static_url_path='/static')
 
 #candidates=json.load(open("candidates.json","r+")) 
-try:
-    candidatesdatabase=db.connect("candidates.db")
-except Error as e:
-    print(e)
-cursor=candidatesdatabase.cursor()
+
 #json_file=open("candidates.json","a+")
 
 @App.route("/",methods=["GET", "POST"])
@@ -21,20 +18,25 @@ def vote_update():
     if request.method=="GET":
         return render_template("vote.html",no_of_votes=voted_per_students)
     elif request.method=="POST":
-        #for row in        
+        try:
+            candidatesdatabase=db.connect("candidate.db")
+        except Error as e:
+            print(e)
+        cursor=candidatesdatabase.cursor()        
         for i in range(1,voted_per_students+1):
             votedcandidate=request.form["CandidateName"+str(i)]
             cursor.execute(f'''SELECT VOTES 
-                           FROM CANDIDATES 
-                           WHERE NAME={votedcandidate};
+                           FROM CANDIDATE
+                           WHERE NAME='{votedcandidate}';
 ''')
             votes=cursor.fetchone()
             if votes:
                 cursor.execute(f'''
-                            UPDATE CANDIDATES
-                            SET VOTES={votes+1}
-                            WHERE NAME={votedcandidate};
-''')        
+                            UPDATE CANDIDATE
+                            SET VOTES={votes[0]+1}
+                            WHERE NAME='{votedcandidate}';
+                ''')            
+                candidatesdatabase.commit()  
             else:
                 return render_template(f"<h1> NO CANDIDATE NAMED {votedcandidate}!!!!!")
 
@@ -42,16 +44,20 @@ def vote_update():
 
 @App.route("/results")
 def results():
+    try:
+        candidatesdatabase=db.connect("candidate.db")
+    except Error as e:
+        print(e)
+    cursor=candidatesdatabase.cursor()
     cursor.execute(f'''
-        SELECT * FROM CANDIDATES
+        SELECT * FROM CANDIDATE
         ORDER BY VOTES DESC;
 ''')
     no_of_candidates=input("Kaç kaptan seçileceğini giriniz:")
     results=cursor.fetchall()
-    succesors=[x for x in results[:no_of_candidates]]
+    succesors=[x for x in results[:int(no_of_candidates)]]
     return render_template("results.html",results=succesors)
 
         
 if __name__ =="__main__":
-    App.run(host=ipv4addr, port="81",threaded=True)
-    
+    App.run(host=ipv4addr, port=81,threaded=True)
